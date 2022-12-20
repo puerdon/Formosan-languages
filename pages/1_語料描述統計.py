dc44c88fbd448534903daeaba56dc382778d38b8
puerdon <dondon726@gmail.com>
2022-12-20 15:53:02 +0800

    test

--- pages/1_語料描述統計.py
+++ pages/1_語料描述統計.py
@@ -2,19 +2,19 @@ import streamlit as st
 
 st.markdown("# 語料描述統計")
 
-# # Cache the raw data and profile report to speed up subseuqent requests 
-# @st.cache
-# def get_data():
-#   # df = pd.read_pickle('Formosan-Mandarin_sent_pairs_139023entries.pkl')
-#   df = pd.read_pickle('../data/Formosan-Mandarin_sent_pairs_20221219.pkl', compression="gzip")
-#   df = df.astype(str, errors='ignore')
-#   df = df.applymap(lambda x: x[1:] if x.startswith(".") else x)
-#   df = df.applymap(lambda x: x.strip())
-#   filt = df.Ch.apply(len) < 5
-#   df = df[~filt]
-#   return df
+# Cache the raw data and profile report to speed up subseuqent requests 
+@st.cache
+def get_data():
+  # df = pd.read_pickle('Formosan-Mandarin_sent_pairs_139023entries.pkl')
+  df = pd.read_pickle('../data/Formosan-Mandarin_sent_pairs_20221219.pkl', compression="gzip")
+  df = df.astype(str, errors='ignore')
+  df = df.applymap(lambda x: x[1:] if x.startswith(".") else x)
+  df = df.applymap(lambda x: x.strip())
+  filt = df.Ch.apply(len) < 5
+  df = df[~filt]
+  return df
 
-# df = get_data()
+df = get_data()
 
 df['word_count'] = df['Ab'].str.split(' ').str.len()
 df['sent_count'] = df['Ab'].astype('str').apply(lambda s: len(sent_tokenize(s)))
