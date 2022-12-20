b877142c702fe8950f87ea23ac2da71bedb0f129
puerdon <dondon726@gmail.com>
2022-12-20 16:34:59 +0800

    update

--- pages/1_語料描述統計.py
+++ pages/1_語料描述統計.py
@@ -1,13 +1,11 @@
 import streamlit as st
 import pandas as pd
-from nltk import sent_tokenize
 
 st.markdown("# 語料描述統計")
 
 # Cache the raw data and profile report to speed up subseuqent requests 
 @st.cache
 def get_basic_stats():
-  # df = pd.read_pickle('Formosan-Mandarin_sent_pairs_139023entries.pkl')
   d = pd.read_pickle('data/basic_stats.pkl', compression="gzip")
   return d
 
