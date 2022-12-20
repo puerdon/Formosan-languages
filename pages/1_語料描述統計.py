6fdeb6b4e4acc7856e16679524e9ba2535793bd6
puerdon <dondon726@gmail.com>
2022-12-20 16:26:25 +0800

    update

--- pages/1_語料描述統計.py
+++ pages/1_語料描述統計.py
@@ -8,8 +8,8 @@ st.markdown("# 語料描述統計")
 @st.cache
 def get_basic_stats():
   # df = pd.read_pickle('Formosan-Mandarin_sent_pairs_139023entries.pkl')
-  df = pd.read_pickle('data/basic_stats.pkl', compression="gzip")
-  return df
+  d = pd.read_pickle('data/basic_stats.pkl', compression="gzip")
+  return d
 
 df_basic_stats = get_basic_stats()
 
