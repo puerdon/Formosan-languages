2d46aed2826da0a3e5af29cfb294f24a2f7cd0bd
puerdon <dondon726@gmail.com>
2022-12-27 11:46:07 +0800

    update

--- pages/1_語料描述統計.py
+++ pages/1_語料描述統計.py
@@ -3,15 +3,27 @@ import pandas as pd
 
 st.markdown("# 語料描述統計")
 
+# CSS to inject contained in a string
+hide_table_row_index = """
+            <style>
+            thead tr th:first-child {display:none}
+            tbody th {display:none}
+            </style>
+            """
+
+# Inject CSS with Markdown
+st.markdown(hide_table_row_index, unsafe_allow_html=True)
+
+
 # Cache the raw data and profile report to speed up subseuqent requests 
 @st.cache
 def get_basic_stats():
-  d = pd.read_pickle('data/stats_of_each_text.pkl', compression="gzip")
+  d = pd.read_pickle('data/stats_of_each_text2.pkl', compression="gzip")
   return d
 
 @st.cache
 def get_each_text_stats():
-  d = pd.read_pickle('data/stats_of_text_with_stories.pkl', compression="gzip")
+  d = pd.read_pickle('data/stats_of_text_with_stories2.pkl', compression="gzip")
   return d
 
 df_basic_stats = get_basic_stats()
@@ -20,8 +32,8 @@ each_text_with_stories = get_each_text_stats()
 
 st.markdown("## 各篇文本詞句數統計")
 
-df_basic_stats
+st.table(df_basic_stats)
 
 st.markdown("## 含單篇子文本的詞句數統計")
 
-each_text_with_stories
+st.table(each_text_with_stories)
